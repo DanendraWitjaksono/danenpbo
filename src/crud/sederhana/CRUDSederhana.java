2d954255cee3c7ee0a63b4bde3acdbc35633f26d
DanendraWitjaksono <dndra.wj@gmail.com>
2022-11-17 15:00:59 -0800

    first commit

---
+++ src/crud/sederhana/CRUDSederhana.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud.sederhana;
+import java.sql.*;
+import javax.swing.*;;
+
+/**
+ *
+ * @author Lab 1
+ */
+public class CRUDSederhana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/crud/sederhana/koneksi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud.sederhana;
+
+import java.sql.Statement;
+
+/**
+ *
+ * @author Lab 1
+ */
+public class koneksi {
+
+    static Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
